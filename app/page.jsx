66ccb092a483a86de5f8d274530a25b3a038ce90
Oshoaro Favour <oshoarofavour@gmail.com>
2024-05-16 00:49:12 +0100

    changed the font-style & weight of the texts

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div>Hello World</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusamus itaque tempora dolore officiis molestiae nam velit, atque, praesentium soluta voluptas, id facere at ad repellendus perspiciatis illo ipsam magni!</div>
     </main>
   );
 }
