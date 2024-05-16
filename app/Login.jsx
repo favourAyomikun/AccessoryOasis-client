58efed5dc3578b9dbafbfc1f34d35d4749745f45
Oshoaro Favour <oshoarofavour@gmail.com>
2024-05-16 02:07:10 +0100

    designed the login form

---
+++ app/Login.jsx
@@ -0,0 +1,27 @@
+'use-client'
+
+import React from "react";
+
+export default function Login() {
+  return (
+    <main className="pt-40">
+      <form className="max-w-md mx-auto bg-gray-300 shadow-lg rounded-lg border-2 border-gray-500 p-6">
+        <div className="input_container">
+          <label htmlFor="email" className="label">
+            Email
+          </label>
+          <input type="email" className="input" />
+        </div>
+        <div className="input_container">
+          <label htmlFor="password" className="label">
+            Password
+          </label>
+          <input type="password" className="input" />
+        </div>
+        <button className="mt-5 bg-[#4169E1] text-white font-bold py-2 px-4 rounded-md w-24">
+          Login
+        </button>
+      </form>
+    </main>
+  );
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,9 @@
+import Login from "./Login";
+
 export default function Home() {
   return (
-    <main>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusamus itaque tempora dolore officiis molestiae nam velit, atque, praesentium soluta voluptas, id facere at ad repellendus perspiciatis illo ipsam magni!</div>
+    <main className="bg-[#E5E5E5] h-screen">
+      <Login />
     </main>
   );
 }
