271419934679859b299e0ed2a76f9e6deff4dd90
Oshoaro Favour <oshoarofavour@gmail.com>
2024-08-07 20:01:20 +0100

    deleted images from the public folder

---
+++ data/AccessoriesData.js
@@ -0,0 +1,51 @@
+const accessoriesData = [
+    {
+      "id": 1,
+      "name": "Elegant Gold Necklace",
+      "description": "A stunning gold necklace that adds a touch of elegance to any outfit.",
+      "price": 79.99,
+      "category": "Jewelry",
+      "image_url": "/images/gold_necklace.jpg"
+    },
+    {
+      "id": 2,
+      "name": "Classic Leather Handbag",
+      "description": "A classic leather handbag that is both stylish and functional.",
+      "price": 149.99,
+      "category": "Bags",
+      "image_url": "/images/leather_handbag.jpg"
+    },
+    {
+      "id": 3,
+      "name": "Stylish Aviator Sunglasses",
+      "description": "Protect your eyes with these trendy aviator sunglasses.",
+      "price": 49.99,
+      "category": "Sunglasses",
+      "image_url": "/images/aviator_sunglasses.jpg"
+    },
+    {
+      "id": 4,
+      "name": "Elegant Silver Bracelet",
+      "description": "A beautiful silver bracelet that complements any outfit.",
+      "price": 59.99,
+      "category": "Jewelry",
+      "image_url": "/images/silver_bracelet.jpg"
+    },
+    {
+      "id": 5,
+      "name": "Casual Fedora Hat",
+      "description": "A stylish fedora hat that adds a casual yet sophisticated touch.",
+      "price": 39.99,
+      "category": "Hats",
+      "image_url": "/images/fedora_hat.jpg"
+    },
+    {
+      "id": 6,
+      "name": "Warm Knit Scarf",
+      "description": "Stay cozy and stylish with this warm knit scarf.",
+      "price": 29.99,
+      "category": "Scarves",
+      "image_url": "/images/knit_scarf.jpg"
+    }
+  ];
+  
