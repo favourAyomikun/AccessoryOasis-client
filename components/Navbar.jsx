9fc67d24bd7dadab88bb1f9ef2a901dd648b6e18
Oshoaro Favour <oshoarofavour@gmail.com>
2024-09-08 20:16:07 +0100

    worked on the respnsivenss for the navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -26,24 +26,26 @@ const Navbar = () => {
     <nav className="container max-w-full h-14 fixed flex items-center justify-around bg-[#1F3A93]">
       <Link
         href={"/homepage"}
-        className={`${kellyslab.className} text-[#E6A8A1] text-[33px]`}
+        className={`${kellyslab.className} text-[#E6A8A1] text-[30px] md:text-[33px]`}
       >
         AccessoryOasis
       </Link>
-      <Link href="/homepage/cartPage" className="relative">
-        <HiOutlineShoppingCart className="text-[35px] text-white" />
-        {totalQuantity > 0 && (
-          <span className="absolute -top-1 left-7 bg-#E6A8A1] text-white text-sm rounded-full px-2">
-            {totalQuantity}
-          </span>
-        )}
-      </Link>
-      <button
-        onClick={handleSignOut}
-        className={`${kellyslab.className} text-[#E6A8A1] text-[23px]`}
-      >
-        Log Out
-      </button>
+      <div className="flex space-x-2 md:space-x-5">
+        <Link href="/homepage/cartPage" className="relative">
+          <HiOutlineShoppingCart className="text-[30px] md:text-[35px] text-white" />
+          {totalQuantity > 0 && (
+            <span className="absolute -top-1 left-7 bg-#E6A8A1] text-white text-sm rounded-full px-2">
+              {totalQuantity}
+            </span>
+          )}
+        </Link>
+        <button
+          onClick={handleSignOut}
+          className={`${kellyslab.className} text-[#E6A8A1] text-[21px] md:text-[23px]`}
+        >
+          Log Out
+        </button>
+      </div>
     </nav>
   );
 };
